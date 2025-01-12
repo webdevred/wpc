c92afd020762d85e889578f18c07eca0ce8587be
webdevred <webdevred>
2025-01-12 21:16:43 +0100

    formatted code

--- wpc.c
+++ wpc.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 static gchar **list_images(gchar *source_directory) {
     gchar **filenamesArray = NULL;
     int filenameArraySize = 0;
@@ -33,7 +32,8 @@ static gchar **list_images(gchar *source_directory) {
         }
         closedir(dir);
 
-        filenamesArray = realloc(filenamesArray, (filenameArraySize + 1) * sizeof(gchar *));
+        filenamesArray =
+            realloc(filenamesArray, (filenameArraySize + 1) * sizeof(gchar *));
         filenamesArray[filenameArraySize] = NULL;
     }
 
@@ -52,9 +52,13 @@ static void show_image(GtkWidget *image, gchar *image_path) {
         double scale = (double)new_width / (double)original_width;
         int new_height = (int)(original_height * scale);
 
-        g_print("Image path: %-40s Original resolution: %dx%d New resolution: %dx%d\n", image_path, original_width, original_height, new_width, new_height);
+        g_print("Image path: %-40s Original resolution: %dx%d New resolution: "
+                "%dx%d\n",
+                image_path, original_width, original_height, new_width,
+                new_height);
 
-        GdkPixbuf *scaled_pixbuf = gdk_pixbuf_scale_simple(pixbuf, new_width, new_height, GDK_INTERP_BILINEAR);
+        GdkPixbuf *scaled_pixbuf = gdk_pixbuf_scale_simple(
+            pixbuf, new_width, new_height, GDK_INTERP_BILINEAR);
 
         gtk_image_set_from_pixbuf(GTK_IMAGE(image), scaled_pixbuf);
 
@@ -62,9 +66,9 @@ static void show_image(GtkWidget *image, gchar *image_path) {
     }
 }
 
-
-static void image_clicked(GtkWidget *widget, GdkEventButton *event, gpointer user_data) {
-    gchar* image_path = (gchar *)user_data;
+static void image_clicked(GtkWidget *widget, GdkEventButton *event,
+                          gpointer user_data) {
+    gchar *image_path = (gchar *)user_data;
     g_print("Clicked image %s\n", image_path);
 }
 
@@ -74,35 +78,39 @@ static void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
     gtk_container_add(GTK_CONTAINER(window), hbox);
 
-    gchar* source_directory =  "/mnt/HDD/backgrounds/";
+    gchar *source_directory = "/mnt/HDD/backgrounds/";
     gchar **images = list_images(source_directory);
 
     if (images) {
-      for (int i = 0; images[i] != NULL; i++) {
-        GtkWidget *event_box;
-        GtkWidget *image = gtk_image_new();
+        for (int i = 0; images[i] != NULL; i++) {
+            GtkWidget *event_box;
+            GtkWidget *image = gtk_image_new();
 
-        event_box = gtk_event_box_new ();
+            event_box = gtk_event_box_new();
 
-        gtk_container_add (GTK_CONTAINER(event_box), image);
-        gtk_box_pack_start (GTK_BOX (hbox), event_box, TRUE, TRUE, 0);
+            gtk_container_add(GTK_CONTAINER(event_box), image);
+            gtk_box_pack_start(GTK_BOX(hbox), event_box, TRUE, TRUE, 0);
 
-        
-        gchar *image_path = malloc(strlen(source_directory) * sizeof(gchar*) + strlen(images[i]) * sizeof(gchar*) + sizeof(gchar *));
+            gchar *image_path =
+                malloc(strlen(source_directory) * sizeof(gchar *) +
+                       strlen(images[i]) * sizeof(gchar *) + sizeof(gchar *));
 
-        image_path = g_strdup( g_strconcat(source_directory, images[i], NULL));
-        
-        show_image(image, image_path);
-        
-        g_signal_connect_data(G_OBJECT(event_box), "button_press_event", G_CALLBACK(image_clicked), (gpointer)image_path, NULL, 0);
+            image_path =
+                g_strdup(g_strconcat(source_directory, images[i], NULL));
 
-        free(images[i]);
-        g_free(image_path);
-      }
+            show_image(image, image_path);
 
-      free(images);
+            g_signal_connect_data(G_OBJECT(event_box), "button_press_event",
+                                  G_CALLBACK(image_clicked),
+                                  (gpointer)image_path, NULL, 0);
+
+            free(images[i]);
+            g_free(image_path);
+        }
+
+        free(images);
     } else {
-      g_print("No images found in %s\n",source_directory);
+        g_print("No images found in %s\n", source_directory);
     }
 
     gtk_widget_show_all(GTK_WIDGET(window));

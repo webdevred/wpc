c7514afc2ab53eebecfe4a6057630598e10fe3c2
webdevred <148627186+webdevred@users.noreply.github.com>
2025-06-03 20:29:07 +0200

    added imagemagick 7 support to the nob

--- nob.c
+++ nob.c
@@ -16,9 +16,12 @@ typedef struct {
     char **items;
 } LibFlagsDa;
 
+bool use_imagemagick7 = false;
+
 void build_object(Nob_Cmd *cmd, LibFlagsDa *main_flags,
                   LibFlagsDa *common_flags) {
     char *lib = nob_temp_sprintf("-I%s", HEADER_FOLDER);
+    if (use_imagemagick7) nob_cmd_append(cmd, "-DWPC_IMAGEMAGICK_7=1");
     nob_cmd_append(
         cmd, "-std=gnu2x", "-DWPC_ENABLE_HELPER=1",
         "-DWPC_HELPER_PATH=\"/usr/local/libexec/wpc/lightdm_helper\"");
@@ -98,24 +101,25 @@ Nob_File_Paths build_source_files(Nob_Cmd *cmd, const char *target,
         uint file_path_len = strlen(files.items[i]);
         if (strncmp(filename, "#", 1) != 0 && strncmp(filename, ".", 1) != 0 &&
             filename[file_path_len - 1] != '~') {
-            char* object = strdup((char *)files.items[i]);
+            char *object = strdup((char *)files.items[i]);
             char *ext = strrchr(object, '.');
             ext++;
             *ext = 'o';
             char *object_place =
                 nob_temp_sprintf("%s/%s", BUILD_FOLDER, object);
             if (strcmp(target, "wpc") == 0) {
-              if ( strcmp(object, "wpc_lightdm_helper.o") != 0) {
-                nob_da_append(&objects, object_place);
-              } else {
-                continue;
-              }
+                if (strcmp(object, "wpc_lightdm_helper.o") != 0) {
+                    nob_da_append(&objects, object_place);
+                } else {
+                    continue;
+                }
             } else if (strcmp(target, "wpc_lightdm_helper") == 0) {
-              if ( strcmp(object, "common.o") == 0 || strcmp(object, "wpc_lightdm_helper.o") == 0) {
-                nob_da_append(&objects, object_place);
-              } else {
-                continue;
-              }
+                if (strcmp(object, "common.o") == 0 ||
+                    strcmp(object, "wpc_lightdm_helper.o") == 0) {
+                    nob_da_append(&objects, object_place);
+                } else {
+                    continue;
+                }
             }
 
             nob_cc(cmd);
@@ -154,12 +158,52 @@ int build_target(Nob_Cmd *cmd, const char *target, Nob_File_Paths objects,
     return 0;
 }
 
+void should_use_imagemagick7(Nob_Cmd *cmd) {
+    char* imagemagick_version_env = getenv("WPC_IMAGEMAGICK_7");
+    if (imagemagick_version_env != NULL) {
+        use_imagemagick7 = strcmp(imagemagick_version_env, "0") != 0;
+        goto log;
+    }
+
+    nob_log(NOB_INFO,"Checking ImageMagick version");
+    nob_cmd_append(cmd, "pkg-config", "--modversion", "MagickWand");
+
+    int pipefd[2];
+    pipe(pipefd);
+
+    int fdout = pipefd[1];
+
+    Nob_Cmd_Redirect cmd_red = {0};
+    cmd_red.fdout = &fdout;
+    nob_cmd_run_async_redirect(*cmd, cmd_red);
+    cmd->count = 0;
+
+    char buffer[1024];
+    read(pipefd[0], buffer, sizeof(buffer) - 1);
+    close(pipefd[0]);
+
+    if (strncmp(buffer, "7.", 2) == 0) {
+        use_imagemagick7 = true;
+    } else if (strncmp(buffer, "6.", 2) == 0) {
+        use_imagemagick7 = false;
+    } else {
+      nob_log(NOB_ERROR, "Couldnt find good ImageMagick. We support ImageMagick 6 and 7");
+      exit(1);
+    }
+    cmd->count = 0;
+ log:
+    char imagemagick_version = use_imagemagick7 ? '7' : '6';
+    nob_log(NOB_INFO, "Decided use to ImageMagick %c", imagemagick_version);
+    return;
+}
+
 int main(int argc, char **argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
 
     if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return 1;
 
     Nob_Cmd cmd = {0};
+    should_use_imagemagick7(&cmd);
 
     char *wpc_libs[] = {"gtk4",       "glib-2.0", "x11", "xrandr",
                         "MagickWand", "libmagic", NULL};
